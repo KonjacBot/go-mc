0905d4af19c2a0d6063bad2e2d4768c66365d87a
patyhank <mlpmmy@gmail.com>
2026-06-29 00:01:32 +0800

    remove unused errors

--- data/registryid/bootstrap/builtinregistries.go
+++ data/registryid/bootstrap/builtinregistries.go
@@ -1,17 +1,1 @@
 package bootstrap
-
-import (
-	"github.com/KonjacBot/go-mc/data/registryid"
-	"github.com/KonjacBot/go-mc/level/block"
-	"github.com/KonjacBot/go-mc/registry"
-)
-
-func RegisterBlocks(reg *registry.Registry[block.Block]) {
-	reg.Clear()
-	for i, key := range registryid.Block {
-		id, val := reg.Put(key, block.FromID[key])
-		if int32(i) != id || val == nil || *val == nil {
-			panic("register blocks failed")
-		}
-	}
-}
